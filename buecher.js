b33c6aecc4e2b82bed57aca04c4c0ee91d17bba4
werner <werner@kurs.de>
2013-11-15 14:53:48 +0100

    format list of books

--- buecher.js
+++ buecher.js
@@ -10,14 +10,29 @@ $(document).ready(function() {
 			success: function(data) {
 			$("#buchvorschau").html("");
 				$.each(data.items, function(index, book){
-					$("#buchvorschau").append("<p>"+book.volumeInfo.title+"</p>");
-					$.mobile.loading("hide");
+					$("#buchvorschau").append("<li>"+buchZuHtml(book)+"</li>");
 				});
+				$("#buchvorschau").listview("refresh");
+				$.mobile.loading("hide");
 			},
 			error: function(data) {
 				$.mobile.loading("hide");
-				$("#buchvorschau").html("Daten konnten nicht geladen werden.");
+				$("#buchvorschau").html("<li>Daten konnten nicht geladen werden.</li>");
 			}
 		});
 	});
-f});
+	
+	function buchZuHtml(buch){
+		$("#buchvorlage .titel").html(buch.volumeInfo.title);
+		
+		if (buch.volumeInfo.authors != undefined) {
+			$("#buchvorlage .autoren").html(buch.volumeInfo.authors.join(", "));
+		} else {
+			$("#buchvorlage .autoren").html("Autor unbekannt");
+		}
+		
+		$("#buchvorlage .beschreibung").html(buch.volumeInfo.description);
+		$("#buchvorlage .bild").attr("src", buch.volumeInfo.imageLinks.smallThumbnail);
+		return $("#buchvorlage").html();
+	}
+});
